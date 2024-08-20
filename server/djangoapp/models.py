02cd068a4cfb4be8342e6fc390ed515bd45813f0
kurt <kmsimera@gmail.com>
2024-08-20 16:52:27 +0800

    fix linting errors

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -12,10 +12,12 @@ class CarMake(models.Model):
 
     def __str__(self):
         # Return the name as the string representation
-        return self.name  
+        return self.name
+
 
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    # Many-to-One relationship
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -25,12 +27,12 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
-        validators=[
-            MaxValueValidator(2023),
-            MinValueValidator(2015)
-        ])
+            validators=[
+                MaxValueValidator(2023),
+                MinValueValidator(2015)
+            ])
     # Other fields as needed
 
     def __str__(self):
         # Return the name as the string representation
-        return self.name  
+        return self.name
